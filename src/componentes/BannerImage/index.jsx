2a574acdb081c52737a52ac1ceea7b9272f1a1d4
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2023-05-04 21:55:24 -0300

    ajustes banner_image

--- src/componentes/BannerImage/index.jsx
+++ src/componentes/BannerImage/index.jsx
@@ -7,7 +7,7 @@ const Img = styled.img`
 
 function BannerImage () {
     return (
-        <Img src="../../../dist/img/banner_image.png" alt="Banner"/>
+        <Img src="/img/banner_image.png" alt="Banner"/>
     )
    
 }

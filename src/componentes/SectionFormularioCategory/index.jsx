7042fe8f7357c5abb7dc3fcaaaa2eb25d04144ea
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2023-05-17 13:18:07 -0300

    validação do formulário de nova categoria

--- src/componentes/SectionFormularioCategory/index.jsx
+++ src/componentes/SectionFormularioCategory/index.jsx
@@ -8,10 +8,10 @@ const Form = styled.form`
     display: flex;
     align-items: center;
     justify-content: center;
-    height: 700px;
+    height: 800px;
 
     @media (min-width: 993px) {
-        height: 600px;
+        height: 700px;
     }
 `;
 
@@ -33,13 +33,15 @@ const P = styled.p`
   color: ${variaveis.corGrayLighter};
   font-weight: bold;
   font-size: 20px;
-  margin: 20px 0;
+  margin: 30px 0;
+  text-align: left;
 `;
 
 const ButtonContainer = styled.div`
     display: flex;
     gap: 30px;
     flex-direction: column;
+    margin: 20px 0;
 
     @media (min-width: 993px) {
         flex-direction: row;
@@ -47,7 +49,7 @@ const ButtonContainer = styled.div`
 `;
 
 const Span =styled.span`
-    color: ${variaveis.corErroDark};
+  color: ${variaveis.corErroDark};
 `;
 
 function createData(
@@ -75,7 +77,11 @@ function SectionFormularioCategory ({ titulo, variaveis }) {
     const [selectedValue, setSelectedValue] = useState('');
     const [descricao, setDescricao] = useState('');
     const [codigoSeguranca, setCodigoSeguranca] = useState('');
+    const [categoryError, setCategoryError] = useState('');
+    const [selectedValueError, setSelectedValueError] = useState('');
+    const [codigoSegurancaError, setCodigoSegurancaError] = useState('');
     const [categories, setCategories] = useState({});
+    const [formSubmitted, setFormSubmitted] = useState(false);
 
     useEffect(() => {
       fetchCategories();
@@ -98,7 +104,57 @@ function SectionFormularioCategory ({ titulo, variaveis }) {
         });
     };
   
-    const handleCreateCategory = () => {
+    const handleCategoryChange = (event) => {
+      if (event.target.value.trim() !== '') {
+        setCategory(event.target.value);
+      } else {
+        setCategoryError('O campo não pode ser vazio');
+      }
+    };
+    
+    const handleDescricaoChange = (event) => {
+      setDescricao(event.target.value);
+    };
+    
+    const handleCodigoSegurancaChange = (event) => {
+      if (event.target.value.trim() !== '') {
+        setCodigoSeguranca(event.target.value);
+      } else {
+        setCodigoSegurancaError('O campo não pode ser vazio');
+      }
+    };
+    
+    const handleSelectChange = (event) => {
+      if (event.target.value.trim() !== '') {
+        setSelectedValue(event.target.value);
+      } else {
+        setSelectedValueError('O campo não pode ser vazio');
+      }
+    };
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+    
+      let categoryError = '';
+      let selectedValueError = '';
+      let codigoSegurancaError = '';
+    
+      if (category.trim() === '') {
+        categoryError = 'O campo não pode ser vazio';
+      }
+    
+      if (selectedValue.trim() === '') {
+        selectedValueError = 'O campo não pode ser vazio';
+      }
+    
+      if (codigoSeguranca.trim() === '') {
+        codigoSegurancaError = 'O campo não pode ser vazio';
+      }
+    
+      setCategoryError(categoryError);
+      setSelectedValueError(selectedValueError);
+      setCodigoSegurancaError(codigoSegurancaError);
+
       const newCategory = {
         [category]: [
           {
@@ -130,35 +186,17 @@ function SectionFormularioCategory ({ titulo, variaveis }) {
         .catch((error) => {
           console.error("Erro ao criar nova categoria:", error);
         });
-    };
-
     
-
-    const handleCategoryChange = (event) => {
-      setCategory(event.target.value);
+      if (categoryError === '' && selectedValueError === '' && codigoSegurancaError === '') {
+        console.log('Valor do Categoria:', category);
+        console.log('Valor selecionado:', selectedValue);
+        console.log('Valor da Descrição:', descricao);
+        console.log('Valor do Código de Segurança:', codigoSeguranca);
+    
+        setFormSubmitted(true);
+      }
     };
-  
-    const handleDescricaoChange = (event) => {
-      setDescricao(event.target.value);
-    };
-  
-    const handleCodigoSegurancaChange = (event) => {
-      setCodigoSeguranca(event.target.value);
-    };
-  
-    const handleSubmit = (event) => {
-      event.preventDefault();
-  
-      console.log('Valor do Título:', title);
-      console.log('Valor selecionado:', selectedValue);
-      console.log('Valor da Descrição:', descricao);
-      console.log('Valor do Código de Segurança:', codigoSeguranca);
-
-    };
-
-    const handleSelectChange = (event) => {
-        setSelectedValue(event.target.value);
-    };
+    
 
     const categoriaCores = {
         Inovação: {
@@ -191,44 +229,67 @@ function SectionFormularioCategory ({ titulo, variaveis }) {
     
       return (
         <>
-        <Form onSubmit={handleSubmit}>
-          <FormControl>
-            <Legend>{titulo}</Legend>
-            <CampoTexto placeholder="Categoria" type="text" value={category}
-          onChange={handleCategoryChange} required />
-            <FormControl
-              variant="filled"
-              sx={{ backgroundColor: variaveis.corInput, borderRadius: 1, marginTop: 2, marginBottom: 2 }}
-            >
-              <InputLabel id="demo-simple-select-filled-label">Selecione uma opção</InputLabel>
-              <Select
-                labelId="demo-simple-select-filled-label"
-                id="demo-select"
-                value={selectedValue}
-                onChange={handleSelectChange}
-                sx={{ backgroundColor: variaveis.corInput, borderRadius: 1 }}
+          <Form onSubmit={handleSubmit}>
+            <FormControl>
+              <Legend>{titulo}</Legend>
+              <CampoTexto placeholder="Categoria"
+              type="text" 
+              value={category}
+              onChange={handleCategoryChange} 
+              error={categoryError !== ''}
+              helperText={categoryError}
+              required
+              />
+              {categoryError && <Span>{categoryError}</Span>}
+              <FormControl
+                variant="filled"
+                sx={{ backgroundColor: variaveis.corInput, borderRadius: 1, marginTop: 2, marginBottom: 2 }}
               >
-                {Object.keys(categoriaCores).map((categoria) => (
-                  <MenuItem
-                    key={categoria}
-                    value={categoria}
-                    style={getCategoriaStyles(categoria)}
-                  >
-                    {categoria}
-                  </MenuItem>
-                ))}
-              </Select>
+                <InputLabel id="demo-simple-select-filled-label">Selecione uma opção</InputLabel>
+                <Select
+                  labelId="demo-simple-select-filled-label"
+                  id="demo-select"
+                  value={selectedValue}
+                  onChange={handleSelectChange}
+                  error={selectedValueError !== ''}
+                  helperText={selectedValueError}
+                  required
+                  sx={{ backgroundColor: variaveis.corInput, borderRadius: 1 }}
+                >
+                  {Object.keys(categoriaCores).map((categoria) => (
+                    <MenuItem
+                      key={categoria}
+                      value={categoria}
+                      style={getCategoriaStyles(categoria)}
+                    >
+                      {categoria}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+              {selectedValueError && <Span>{selectedValueError}</Span>}
+              <CampoTexto
+              placeholder="Descrição"
+              type="text" value={descricao}
+              onChange={handleDescricaoChange}
+              />
+              <CampoTexto
+              placeholder="Código de Segurança"
+              type="number"
+              value={codigoSeguranca}
+              onChange={handleCodigoSegurancaChange}
+              required
+              error={codigoSegurancaError !== ''}
+              helperText={codigoSegurancaError}
+              />
+              {codigoSegurancaError && <Span>{codigoSegurancaError}</Span>}
+              <ButtonContainer>
+                <Button variant="contained" onClick={handleSubmit}>Salvar</Button>
+                <Button variant="outlined">Deletar</Button>
+              </ButtonContainer>
+              {formSubmitted && <P>Formulário enviado com sucesso!</P>}
             </FormControl>
-            <CampoTexto placeholder="Descrição" type="text" value={descricao}
-          onChange={handleDescricaoChange} />
-            <CampoTexto placeholder="Código de Segurança" type="number" value={codigoSeguranca}
-          onChange={handleCodigoSegurancaChange} required />
-            <ButtonContainer>
-              <Button variant="contained" onClick={handleCreateCategory}>Salvar</Button>
-              <Button variant="outlined">Deletar</Button>
-            </ButtonContainer>
-          </FormControl>
-        </Form>
+          </Form>
         <TableContainer component={Paper} sx={{ minWidth: 270, maxWidth: 900, marginLeft: 6,'@media (max-width: 993px)': {
             display: 'none',
           },}}>
